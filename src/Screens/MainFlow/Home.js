07cc18e4210a4560a663e85f2c5b2631ad709a09
Muhammad Ali <ali.muhammad751935@gmail.com>
2023-05-11 12:29:39 +0500

    commit

--- src/Screens/MainFlow/Home.js
+++ src/Screens/MainFlow/Home.js
@@ -5,17 +5,26 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+<<<<<<< Updated upstream
 } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import React, { useEffect, useState } from "react";
 import { EvilIcons } from "@expo/vector-icons";
 import { useFocusEffect } from "@react-navigation/native";
+=======
+} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import Card from '../../Components/Card';
+import { SegmentedButtons, Surface } from 'react-native-paper';
+import { EvilIcons } from '@expo/vector-icons';
+>>>>>>> Stashed changes
 
-import BidderApi from "../../api/BidderApi";
+import BidderApi from '../../api/BidderApi';
 const Home = ({ navigation }) => {
   const [products, setProducts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [value, setValue] = React.useState("");
+  const [searchQuery, setSearchQuery] = useState('');
+  const [value, setValue] = React.useState('');
 
   useEffect(() => {
     getData();
@@ -23,9 +32,9 @@ const Home = ({ navigation }) => {
 
   const getData = async () => {
     try {
-      const res = await BidderApi.get("/product/bid/");
+      const res = await BidderApi.get('/product/bid/');
       console.log(
-        "HOME LSIT::",
+        'HOME LSIT::',
         JSON.stringify(res.data.data.allProducts[0].userId.currentCity, null, 2)
       );
       setProducts(res.data.data.allProducts);
@@ -38,16 +47,14 @@ const Home = ({ navigation }) => {
     return product.title.toLowerCase().includes(searchQuery.toLowerCase());
   });
 
-  const handleProductPress = (product) => {
-    navigation.navigate("Product", { product });
-  };
   const handleBidPress = (bidproduct) => {
-    navigation.navigate("BidProduct", { bidproduct });
+    navigation.navigate('BidProduct', { bidproduct });
   };
   const handleFixPress = (fixproduct) => {
-    navigation.navigate("FixProduct", { fixproduct });
+    navigation.navigate('FixProduct', { fixproduct });
   };
 
+<<<<<<< Updated upstream
   const addfav = async (postId) => {
     try {
       const res = await BidderApi.post("/favorite/", { postId });
@@ -118,79 +125,131 @@ const Home = ({ navigation }) => {
                   <Text style={{ fontSize: 16, fontWeight: "bold" }}>X</Text>
                 </TouchableOpacity>
               </View>
-
-              <Image
-                source={require("../Images/Banner.png")}
-                style={{
-                  width: "91.5%",
-                  height: 160,
-                  borderRadius: 10,
-                  alignSelf: "center",
-                  marginTop: 10,
-                }}
-              />
-            </View>
-            <View
+=======
+  function headerComponent() {
+    return (
+      <>
+        <View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginHorizontal: 16,
+              marginTop: 10,
+              backgroundColor: 'white',
+              borderRadius: 10,
+              overflow: 'hidden',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 7,
+              },
+              shadowOpacity: 0.43,
+              shadowRadius: 9.51,
+              elevation: 15,
+            }}
+          >
+            <TextInput
+              placeholder="Search products..."
+              value={searchQuery}
+              onChangeText={(query) => setSearchQuery(query)}
               style={{
-                marginHorizontal: 10,
-                flexDirection: "row",
-                justifyContent: "center",
+                flex: 1,
+                height: 45,
+                paddingHorizontal: 16,
+                fontSize: 16,
+              }}
+            />
+            <TouchableOpacity
+              onPress={() => setSearchQuery('')}
+              style={{
+                backgroundColor: '#ddd',
+                borderRadius: 5,
+                paddingHorizontal: 12,
+                paddingVertical: 8,
+                marginRight: 3,
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
-              <TouchableOpacity
-                onPress={() => handleBidPress()}
-                style={{
-                  paddingVertical: 10,
-                  paddingHorizontal: 10,
-                  marginHorizontal: 10,
-                }}
-              >
-                <View style={{ alignItems: "center" }}>
-                  <Image
-                    source={require("./../Images/Bid.webp")}
-                    style={{ borderRadius: 40, width: 80, height: 80 }}
-                  />
-                  <Text
-                    style={{
-                      marginTop: 10,
-                      fontSize: 13,
-                      color: "#444",
-                      marginBottom: 5,
-                      fontWeight: "300",
-                    }}
-                  >
-                    Bidding Products
-                  </Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => handleFixPress()}
-                style={{
-                  paddingVertical: 10,
-                  paddingHorizontal: 10,
-                  marginHorizontal: 10,
-                }}
-              >
-                <View style={{ alignItems: "center" }}>
-                  <Image
-                    source={require("./../Images/purchase.webp")}
-                    style={{ borderRadius: 40, width: 80, height: 80 }}
-                  />
-                  <Text
-                    style={{
-                      marginTop: 10,
-                      fontSize: 13,
-                      color: "#444",
-                      marginBottom: 5,
-                      fontWeight: "300",
-                    }}
-                  >
-                    Fix Price Products
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </View>
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>X</Text>
+            </TouchableOpacity>
+          </View>
+>>>>>>> Stashed changes
 
+          <Image
+            source={require('../Images/Banner.png')}
+            style={{
+              width: '91.5%',
+              height: 160,
+              borderRadius: 10,
+              alignSelf: 'center',
+              marginTop: 10,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            marginHorizontal: 10,
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => handleBidPress()}
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              marginHorizontal: 10,
+            }}
+          >
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                source={require('./../Images/Bid.webp')}
+                style={{ borderRadius: 40, width: 80, height: 80 }}
+              />
+              <Text
+                style={{
+                  marginTop: 10,
+                  fontSize: 13,
+                  color: '#444',
+                  marginBottom: 5,
+                  fontWeight: '300',
+                }}
+              >
+                Bidding Products
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => handleFixPress()}
+            style={{
+              paddingVertical: 10,
+              paddingHorizontal: 10,
+              marginHorizontal: 10,
+            }}
+          >
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                source={require('./../Images/purchase.webp')}
+                style={{ borderRadius: 40, width: 80, height: 80 }}
+              />
+              <Text
+                style={{
+                  marginTop: 10,
+                  fontSize: 13,
+                  color: '#444',
+                  marginBottom: 5,
+                  fontWeight: '300',
+                }}
+              >
+                Fix Price Products
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+<<<<<<< Updated upstream
             <Text
               style={{
                 fontSize: 20,
@@ -284,6 +343,30 @@ const Home = ({ navigation }) => {
         }}
       />
     </View>
+=======
+        <Text
+          style={{
+            fontSize: 20,
+            marginLeft: 25,
+            fontWeight: '400',
+          }}
+        >
+          Products
+        </Text>
+      </>
+    );
+  }
+
+  return (
+    <FlatList
+      numColumns={2}
+      data={filteredProducts}
+      ListHeaderComponent={headerComponent}
+      renderItem={({ item }) => {
+        return <Card item={item} />;
+      }}
+    />
+>>>>>>> Stashed changes
   );
 };
 
@@ -292,7 +375,7 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 15,
     marginLeft: 10,
     marginRight: 10,
