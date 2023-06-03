adf9f8b034061a9bb996b1a7a987229e74c1e3b9
Umar1999 <100772423+MUmar-1999@users.noreply.github.com>
2023-06-03 23:38:29 +0500

    Update api base url

--- src/api/BidderApi.js
+++ src/api/BidderApi.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import { getItemAsync } from 'expo-secure-store';
+import axios from "axios";
+import { getItemAsync } from "expo-secure-store";
 
-export const BASE_URL = 'http://192.168.10.2:5000';
+export const BASE_URL = "http://api.biddersbay.online";
 
 export default BidderApi = axios.create({
   baseURL: BASE_URL,
 });
 
 BidderApi.interceptors.request.use(async function (config) {
-  const token = await getItemAsync('token');
+  const token = await getItemAsync("token");
   if (!token) return config;
 
-  config.headers['Authorization'] = JSON.parse(token);
+  config.headers["Authorization"] = JSON.parse(token);
 
   return config;
 });
